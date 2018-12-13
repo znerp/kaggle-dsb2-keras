7290f8ce8278669e98446549d016e8507aef0140
znerp <virtual_flame@yahoo.de>
2018-12-13 19:35:50 +0100

    minor changes

--- tutorial_kaggle/utils/test_functions.py
+++ tutorial_kaggle/utils/test_functions.py
@@ -1,2 +1,10 @@
-#sys.path.insert(1, 'utils')
+import sys
+sys.path.insert(1, 'tutorial_kaggle\\utils')
 from functions import *
+
+def test(bla=3, blubb=4):
+    """empty"""
+    print('nattin')
+
+import os
+os.system('python tutorial_kaggle\\utils\\a.py')
